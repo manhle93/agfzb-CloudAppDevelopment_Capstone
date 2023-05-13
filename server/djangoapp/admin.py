7ae39b0432941499e10de46c16f75d993ccebfc9
manhle <manhle@email.com>
2023-05-13 19:08:56 +0700

    model

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
+
 # from .models import related models
 
-
 # Register your models here.
+from .models import CarModel
+from .models import CarMake
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
 
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    
 # Register models here
+admin.site.register(CarModel)
+admin.site.register(CarMake)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -20,6 +26,21 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+class CarModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    TYPE_CHOICES = (
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+    )
+    car_type = models.CharField(max_length=5, choices=TYPE_CHOICES)
+    year = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} ({self.car_make.name})"
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
